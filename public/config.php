559b97a6121a8c431c88c2202c7ad798bd01118c
Gints Murāns <gm@mstuff.org>
2009-02-27 19:28:21 +0200

    Licence agreements added

--- public/config.php
+++ public/config.php
@@ -1,4 +1,25 @@
 <?php
+/*
+    "Frame" - Little PHP Framework
+
+---------------------------------------------------------------------------------
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+---------------------------------------------------------------------------------
+
+    Copyright (C) 2009  Gints Murāns <gm@mstuff.org>
+*/
+
 
 // DATABASE
 $config['db']['string'] = 'mysql:host=localhost;port=;unix_socket=;dbname='; // See PDO documentation: http://php.net/manual/en/pdo.construct.php
