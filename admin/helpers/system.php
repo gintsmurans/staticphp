4c99a256f6519410b6dc0747ab50a08ac0042c65
Gints Murāns <gm@mstuff.org>
2009-04-01 17:47:04 +0300

    Validation library, new directory structure and js.php controller

--- admin/helpers/system.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Start a session
-session_start();
-
-// Send some headers
-header('Content-type: text/html; charset=utf-8');
-
-?>
--- application/views/E404.php
+++
@@ -1,6 +0,0 @@
-<?php load('views/header'); ?>
-
-        <h1>404</h1>
-
-    </body>
-</html>
