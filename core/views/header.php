bcbb7c35d7fed41f93996340521e37c9d6679e91
Gints Murans <gm@gm.lv>
2011-03-30 04:11:58 +0300

    Fix filenames

--- core/views/header.php
+++ core/views/header.php
@@ -3,7 +3,7 @@
   <head>
     <title>StaticPHP</title>
     <style type="text/css">
-        @import '<?php echo BASE_URI; ?>modules/base/style.css';
+        @import '<?php echo BASE_URI; ?>files/default/style.css';
     </style>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   </head>
