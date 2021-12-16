19213317ef8b9b73318e83597b91674ad640a6ab
Gints Murans <gm@gm.lv>
2021-12-16 02:30:16 +0200

    Updated version

--- src/Application/Config/App.php
+++ src/Application/Config/App.php
@@ -1,6 +1,6 @@
 <?php
 
-$config['version'] = 'v1.1.0';
+$config['version'] = 'v1.2.0';
 $config['git_commit_hash'] = '9ecb125bfd28b70720ab1d106da4588ba8b2b6ac';
 $config['git_commit_date'] = '22.02.2019 13:15';
 
